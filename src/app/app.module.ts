ec6f6df948670db2a18608a105b2c070f01ff324
protocols <protocols@mosaic.com>
2016-10-03 08:55:49 +0900

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Page1 } from '../pages/page1/page1';
+import { Page2 } from '../pages/page2/page2';
+import { Page3 } from '../pages/page3/page3';
+
+import { AboutPage } from '../pages/tabs/about/about';
+import { ContactPage } from '../pages/tabs/contact/contact';
+import { HomePage } from '../pages/tabs/home/home';
+import { TabsPage } from '../pages/tabs/tabs';
+
+@NgModule({
+	declarations: [
+		MyApp,
+		Page1,
+		Page2,
+		Page3,
+        AboutPage,
+		ContactPage,
+		HomePage,
+		TabsPage
+	],
+	imports: [
+		IonicModule.forRoot(MyApp)
+	],
+	bootstrap: [IonicApp],
+	entryComponents: [
+		MyApp,
+		Page1,
+		Page2,
+		Page3,
+        AboutPage,
+		ContactPage,
+		HomePage,
+		TabsPage
+	],
+	providers: []
+})
+export class AppModule { }
