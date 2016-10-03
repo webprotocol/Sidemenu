f6fd81c3ce5899fb005badea3ee603cae47d35d9
protocols <protocols@mosaic.com>
2016-10-03 09:38:44 +0900

    detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { IonicApp, IonicModule } from 'ionic-angular';
 import { MyApp } from './app.component';
 import { Page1 } from '../pages/page1/page1';
 import { Page2 } from '../pages/page2/page2';
+import { DetailPage } from '../pages/page2/detail/detail';
 import { Page3 } from '../pages/page3/page3';
 
 import { AboutPage } from '../pages/tabs/about/about';
@@ -15,6 +16,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 		MyApp,
 		Page1,
 		Page2,
+		DetailPage,
 		Page3,
         AboutPage,
 		ContactPage,
@@ -29,6 +31,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 		MyApp,
 		Page1,
 		Page2,
+		DetailPage,
 		Page3,
         AboutPage,
 		ContactPage,
