822fa06495bfae6d28fb37997deaa494028db4dd
java <java>
2016-10-05 11:17:30 +0900

    motion

--- src/pages/nativex/motion/motion.ts
+++ src/pages/nativex/motion/motion.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import * as native from 'ionic-native';
 
 @Component({
 	selector: 'motion',
@@ -12,13 +13,29 @@ export class MotionPage {
 
 	}
 
+	mflag: boolean = false;
+	aflag: boolean = false;
+
+	x_axis: number = 0;
+	y_axis: number = 0;
+	z_axis: number = 9.8;
+
 	onManual() {
+		this.mflag = true;
 		console.log("onManual()...");
+		native.DeviceMotion.getCurrentAcceleration().then((v:native.AccelerationData) => {
+			this.x_axis = v.x;
+			this.y_axis = v.y;
+			this.z_axis = v.z;
+			this.mflag = false;
+		})
 	}
     onStart() {
+		// this.aflag = true;
 		console.log("onStart()...");
 	}
     onStop() {
+		// this.aflag = false;
 		console.log("onStop()...");
 	}
 
